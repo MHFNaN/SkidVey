6c729df8114c235190edfb327f9c76a153c8f8ca
MHFNaN <88169836+MHFNaN@users.noreply.github.com>
2023-08-13 16:12:09 -0700

    Update Module.java

--- src/main/java/me/alpha432/oyvey/features/modules/Module.java
+++ src/main/java/me/alpha432/oyvey/features/modules/Module.java
@@ -188,6 +188,7 @@ public class Module
         RENDER("Render"),
         MOVEMENT("Movement"),
         PLAYER("Player"),
+        SHITCODE("Shitcode"),
         CLIENT("Client");
 
         private final String name;
